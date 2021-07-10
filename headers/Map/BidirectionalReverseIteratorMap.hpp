e589eb6a10681004025c7a7a04f5af1194b31dda
Farhod Rahimov <farhod.rahimov0055@gmail.com>
2021-07-10 13:01:53 +0300

    reverse iterator done, start testing

---
+++ headers/Map/BidirectionalReverseIteratorMap.hpp
@@ -0,0 +1,297 @@
+#ifndef BIDIRECTIONAL_REVERSE_ITERATOR_MAP_HPP
+#define BIDIRECTIONAL_REVERSE_ITERATOR_MAP_HPP
+
+#include "utils.hpp"
+#include "Map.hpp"
+
+// #include "../Stack.hpp"
+// #include "../Vector.hpp"
+
+namespace ft {
+
+	template < class Key, class T >
+	class BidirectionalReverseIteratorMap {
+	public:
+			typedef 	Key										key_type;
+			typedef 	T										mapped_type;
+			typedef 	ft::pair<key_type, mapped_type>			value_type;
+			typedef 	value_type *							pointer;
+			typedef 	std::bidirectional_iterator_tag         iterator_category;
+
+	// ----------------------------------------------------------------------------------------------------------- //
+
+			BidirectionalReverseIteratorMap() {
+				this->_binary_tree = nullptr;
+			};
+			
+			virtual ~BidirectionalReverseIteratorMap() {};
+			
+			BidirectionalReverseIteratorMap(BinaryTree<Key, T> & src) {
+				this->_binary_tree = &src;
+			};
+			
+			BidirectionalReverseIteratorMap(BidirectionalReverseIteratorMap const & src) {
+				*this = src;
+			};
+			
+			BidirectionalReverseIteratorMap & operator = (BidirectionalReverseIteratorMap const & src) {
+				if (this != &src) {
+					this->_binary_tree = src._binary_tree;
+                }
+				return (*this);
+			};
+
+	// ----------------------------------------------------------------------------------------------------------- //
+
+			bool operator == (BidirectionalReverseIteratorMap const & src) const {
+				return (this->_binary_tree->_value == src._binary_tree->_value);
+			};
+
+			bool operator != (BidirectionalReverseIteratorMap const & src) const {
+				return (this->_binary_tree->_value != src._binary_tree->_value);
+			};
+
+	// ----------------------------------------------------------------------------------------------------------- //
+			
+			value_type & operator * (void) {
+				return(*this->_binary_tree->_value);
+			};
+
+			value_type *operator -> (void) {
+				return(this->_binary_tree->_value);
+			};
+
+	// ----------------------------------------------------------------------------------------------------------- //
+
+			friend BidirectionalReverseIteratorMap operator ++ (BidirectionalReverseIteratorMap & it) {
+				it++;
+				return (it);
+			};
+
+			BidirectionalReverseIteratorMap & operator ++ (int)
+			{
+				BinaryTree<Key, T> *tmp(this->_binary_tree);
+
+				if (tmp->isFirstElement()) {
+					return (*this);
+				}
+				else if (tmp->_lh && !tmp->_lh->isFirstElement()) {
+					tmp = tmp->_lh;
+					while (tmp->_rh && !tmp->_rh->isLastElement()) {
+						tmp = tmp->_rh;
+					}
+				}
+				else if (tmp->isLastElement()) {
+					tmp = tmp->_parent;
+				}
+				else {
+					while (	tmp->_value->first >= this->_binary_tree->_value->first && 
+							tmp->_parent && !tmp->_parent->isLastElement() && 
+							!tmp->_parent->isFirstElement()) {
+						tmp = tmp->_parent;
+					}
+					if (tmp->_value->first >= this->_binary_tree->_value->first) {
+						while (tmp->_parent != nullptr) {
+							tmp = tmp->_parent;
+						}
+						while (!tmp->isFirstElement()) {
+							tmp = tmp->_lh;
+						}
+					}
+				}
+				*this = BidirectionalReverseIteratorMap(*tmp);
+				return (*this);
+			};
+
+	// ----------------------------------------------------------------------------------------------------------- //
+
+			friend BidirectionalReverseIteratorMap operator -- (BidirectionalReverseIteratorMap & it) {
+				it--;
+				return (it);
+			};
+			
+			BidirectionalReverseIteratorMap & operator -- (int) {
+				BinaryTree<Key, T> *tmp(this->_binary_tree);
+
+				if (tmp->isLastElement()) {
+					return (*this);
+				}
+				else if (tmp->_rh && !tmp->_rh->isLastElement()) {
+					tmp = tmp->_rh;
+					while (tmp->_lh && !tmp->_lh->isFirstElement()) {
+						tmp = tmp->_lh;
+					}
+				}
+				else if (tmp->isFirstElement()) {
+					if (tmp->_rh->isLastElement())
+						tmp = tmp->_rh;
+					else
+						tmp = tmp->_parent;
+				}
+				else {
+					while (	tmp->_value->first <= this->_binary_tree->_value->first && 
+							tmp->_parent && !tmp->_parent->isFirstElement() && 
+							!tmp->_parent->isLastElement()) {
+						tmp = tmp->_parent;
+					}
+					if (tmp->_value->first <= this->_binary_tree->_value->first) {
+						while (tmp->_parent != nullptr) {
+							tmp = tmp->_parent;
+						}
+						while (!tmp->isLastElement()) {
+							tmp = tmp->_rh;
+						}
+					}
+				}
+				*this = BidirectionalReverseIteratorMap(*tmp);
+				return (*this);
+			};
+
+	private:
+		BinaryTree<Key, T>		*_binary_tree;
+	};
+
+	template < class Key, class T >
+	class BidirectionalReverseIteratorMapConst {
+	public:
+			typedef 	Key										key_type;
+			typedef 	T										mapped_type;
+			typedef 	ft::pair<key_type, mapped_type>			value_type;
+			typedef 	value_type *							pointer;
+			typedef 	std::bidirectional_iterator_tag         iterator_category;
+
+	// ----------------------------------------------------------------------------------------------------------- //
+
+			BidirectionalReverseIteratorMapConst() {
+				this->_binary_tree = nullptr;
+			};
+			
+			virtual ~BidirectionalReverseIteratorMapConst() {};
+			
+			BidirectionalReverseIteratorMapConst(BinaryTree<Key, T> & src) {
+				this->_binary_tree = &src;
+			};
+			
+			BidirectionalReverseIteratorMapConst(BidirectionalReverseIteratorMapConst const & src) {
+				*this = src;
+			};
+			
+			BidirectionalReverseIteratorMapConst & operator = (BidirectionalReverseIteratorMapConst const & src) {
+				if (this != &src) {
+					this->_binary_tree = src._binary_tree;
+                }
+				return (*this);
+			};
+
+	// ----------------------------------------------------------------------------------------------------------- //
+
+			bool operator == (BidirectionalReverseIteratorMapConst const & src) const {
+				return (this->_binary_tree->_value == src._binary_tree->_value);
+			};
+
+			bool operator != (BidirectionalReverseIteratorMapConst const & src) const {
+				return (this->_binary_tree->_value != src._binary_tree->_value);
+			};
+
+	// ----------------------------------------------------------------------------------------------------------- //
+			
+			value_type & operator * (void) {
+				return(*this->_binary_tree->_value);
+			};
+
+			value_type *operator -> (void) {
+				return(this->_binary_tree->_value);
+			};
+
+	// ----------------------------------------------------------------------------------------------------------- //
+
+			friend BidirectionalReverseIteratorMapConst operator ++ (BidirectionalReverseIteratorMapConst & it) {
+				it++;
+				return (it);
+			};
+
+			BidirectionalReverseIteratorMapConst & operator ++ (int)
+			{
+				BinaryTree<Key, T> *tmp(this->_binary_tree);
+
+				if (tmp->isFirstElement()) {
+					return (*this);
+				}
+				else if (tmp->_lh && !tmp->_lh->isFirstElement()) {
+					tmp = tmp->_lh;
+					while (tmp->_rh && !tmp->_rh->isLastElement()) {
+						tmp = tmp->_rh;
+					}
+				}
+				else if (tmp->isLastElement()) {
+					tmp = tmp->_parent;
+				}
+				else {
+					while (	tmp->_value->first >= this->_binary_tree->_value->first && 
+							tmp->_parent && !tmp->_parent->isLastElement() && 
+							!tmp->_parent->isFirstElement()) {
+						tmp = tmp->_parent;
+					}
+					if (tmp->_value->first >= this->_binary_tree->_value->first) {
+						while (tmp->_parent != nullptr) {
+							tmp = tmp->_parent;
+						}
+						while (!tmp->isFirstElement()) {
+							tmp = tmp->_lh;
+						}
+					}
+				}
+				*this = BidirectionalReverseIteratorMapConst(*tmp);
+				return (*this);
+			};
+
+	// ----------------------------------------------------------------------------------------------------------- //
+
+			friend BidirectionalReverseIteratorMapConst operator -- (BidirectionalReverseIteratorMapConst & it) {
+				it--;
+				return (it);
+			};
+			
+			BidirectionalReverseIteratorMapConst & operator -- (int) {
+				BinaryTree<Key, T> *tmp(this->_binary_tree);
+
+				if (tmp->isLastElement()) {
+					return (*this);
+				}
+				else if (tmp->_rh && !tmp->_rh->isLastElement()) {
+					tmp = tmp->_rh;
+					while (tmp->_lh && !tmp->_lh->isFirstElement()) {
+						tmp = tmp->_lh;
+					}
+				}
+				else if (tmp->isFirstElement()) {
+					if (tmp->_rh->isLastElement())
+						tmp = tmp->_rh;
+					else
+						tmp = tmp->_parent;
+				}
+				else {
+					while (	tmp->_value->first <= this->_binary_tree->_value->first && 
+							tmp->_parent && !tmp->_parent->isFirstElement() && 
+							!tmp->_parent->isLastElement()) {
+						tmp = tmp->_parent;
+					}
+					if (tmp->_value->first <= this->_binary_tree->_value->first) {
+						while (tmp->_parent != nullptr) {
+							tmp = tmp->_parent;
+						}
+						while (!tmp->isLastElement()) {
+							tmp = tmp->_rh;
+						}
+					}
+				}
+				*this = BidirectionalReverseIteratorMapConst(*tmp);
+				return (*this);
+			};
+
+	private:
+		BinaryTree<Key, T>		*_binary_tree;
+	};
+};
+
+#endif
--- headers/Map/Map.hpp
+++ headers/Map/Map.hpp
@@ -4,6 +4,7 @@
 #include <memory>
 
 #include "BidirectionalIteratorMap.hpp"
+#include "BidirectionalReverseIteratorMap.hpp"
 #include "utils.hpp"
 
 namespace ft {
@@ -11,27 +12,27 @@ namespace ft {
 	// template < class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<ft::pair <Key,T> > >
 	class map {
 	public:
-				typedef 			Key														key_type;
-				typedef 			T														mapped_type;
-				typedef 			ft::pair<key_type, mapped_type>							value_type;
-				// typedef 			ft::pair<const key_type, mapped_type>					value_type;
+				typedef 			Key																key_type;
+				typedef 			T																mapped_type;
+				typedef 			ft::pair<key_type, mapped_type>									value_type;
+				// typedef 			ft::pair<const key_type, mapped_type>							value_type;
 
-				typedef 			Compare													key_compare;
-				// typedef 			class value_compare										value_compare;
+				typedef 			Compare															key_compare;
+				// typedef 			class value_compare												value_compare;
 				
-				typedef 			Alloc 													allocator_type;
-				typedef typename	allocator_type::reference 								reference;
-				typedef typename	allocator_type::const_reference 						const_reference;
-				typedef typename	allocator_type::pointer 								pointer;
-				typedef typename	allocator_type::const_pointer 							const_pointer;
+				typedef 			Alloc 															allocator_type;
+				typedef typename	allocator_type::reference 										reference;
+				typedef typename	allocator_type::const_reference 								const_reference;
+				typedef typename	allocator_type::pointer 										pointer;
+				typedef typename	allocator_type::const_pointer 									const_pointer;
 				
-				typedef 			ft::BidirectionalIteratorMap<Key, T>					iterator;
-				typedef 			ft::BidirectionalIteratorMapConst<Key const, T const>	const_iterator;
-				// typedef 			ft::BidirectionalIteratorMap<value_type>				reverse_iterator;
-				// typedef 			ft::BidirectionalIteratorMap<value_type const>			const_reverse_iterator;
+				typedef 			ft::BidirectionalIteratorMap<Key, T>							iterator;
+				typedef 			ft::BidirectionalIteratorMapConst<Key const, T const>			const_iterator;
+				typedef 			ft::BidirectionalReverseIteratorMap<Key, T>						reverse_iterator;
+				typedef 			ft::BidirectionalReverseIteratorMapConst<Key const, T const>	const_reverse_iterator;
 
-				typedef 			std::ptrdiff_t											difference_type;
-				typedef 			size_t													size_type;
+				typedef 			std::ptrdiff_t													difference_type;
+				typedef 			size_t															size_type;
 
 	private:
 				class value_compare : std::binary_function<value_type, value_type, bool> {
@@ -148,8 +149,41 @@ namespace ft {
 					return (const_iterator(*tmp));
 				};
 
-		// 		// iterator rbegin() const {};    // uncomment  uncomment  uncomment  uncomment  uncomment  uncomment 
-		// 		// iterator rend() const {}; //  uncomment  uncomment  uncomment  uncomment  uncomment  uncomment 	
+				reverse_iterator rbegin()  {
+					BinaryTree<key_type, mapped_type> *tmp = this->_root;
+
+					while (!tmp->isLastElement()) {
+						tmp = tmp->_rh;
+					}
+					return (reverse_iterator(*tmp));
+				};
+				
+				const_reverse_iterator rbegin() const {
+					BinaryTree<key_type, mapped_type> *tmp = this->_root;
+
+					while (!tmp->isLastElement()) {
+						tmp = tmp->_rh;
+					}
+					return (const_reverse_iterator(*tmp));
+				};
+
+				reverse_iterator rend() {
+					BinaryTree<key_type, mapped_type> *tmp = this->_root;
+
+					while (tmp && tmp->_lh && !tmp->_lh->isFirstElement()) {
+						tmp = tmp->_lh;
+					}
+					return (reverse_iterator(*tmp));
+				};
+
+				const_reverse_iterator rend() const {
+					BinaryTree<key_type, mapped_type> *tmp = this->_root;
+
+					while (tmp && tmp->_lh && !tmp->_lh->isFirstElement()) {
+						tmp = tmp->_lh;
+					}
+					return (const_reverse_iterator(*tmp));
+				};
 
 		// CAPACITY CAPACITY CAPACITY CAPACITY CAPACITY CAPACITY CAPACITY CAPACITY CAPACITY CAPACITY CAPACITY CAPACITY //
 
